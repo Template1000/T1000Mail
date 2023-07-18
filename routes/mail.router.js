4837ea3c369357b32886cde55e1a0809d06129ed
Template1000 <template1000@proton.me>
2023-07-18 15:26:00 -0500

    [FIX] Modified mail Send mail

--- routes/mail.router.js
+++ routes/mail.router.js
@@ -12,8 +12,8 @@ router.post('/send',
   validatorHandler(sendMailSchema, 'body'),
   async (req, res, next) => {
     try {
-      const { name, email, phone, message } = req.body
-      const response = await service.sendMail(name, email, phone, message)
+      const { name, email, phone, description } = req.body
+      const response = await service.sendMail(name, email, phone, description)
       res.json(response)
     } catch (error) {
       next(error);
