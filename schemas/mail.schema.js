d6953613a9065745c5402fde4bb86255d0ecac4e
Template1000 <template1000@proton.me>
2023-07-26 00:41:46 -0500

    [FIX]

--- schemas/mail.schema.js
+++ schemas/mail.schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const name = Joi.string().pattern(new RegExp('^[ a-zA-Z]')).min(3).max(30)
 const email = Joi.string().email()
 const phone = Joi.number().integer().min(999999999).max(9999999999)
-const description = Joi.string().pattern(/^[ a-zA-Z0-9,.$]+$/)
+const description = Joi.string().pattern(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ0-9,.$]+$/)
 
 const sendMailSchema = Joi.object({
   name: name.required(),
